ea5a21893dcc02ad95542b3a35dee7f6b879b71f
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-08-09 16:07:18 +0600

    product create, get, update, delete done

--- server/controller/productController.js
+++ server/controller/productController.js
@@ -21,4 +21,33 @@ const getallProducts = async (req, res) => {
   }
 };
 
-module.exports = { getallProducts, createProduct };
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    let product = await Product.findById(id);
+    if (!product) {
+      res.status(500).json({ Message: "Product not found" });
+    }
+    product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+    res.status(200).json({ Message: "Updte Successful", product });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(203).json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  getallProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- server/routers/Products.js
+++ server/routers/Products.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const {
   getallProducts,
   createProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controller/productController");
 
 router.get("/", getallProducts);
 router.post("/", createProduct);
+router.put("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
 
 module.exports = router;
